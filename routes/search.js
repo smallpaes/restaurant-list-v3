fe9111cc152fb734236c7fadb6aedb3439c1625a
Mike <mike.huang.mikank@gmail.com>
2019-06-23 18:30:20 +0800

    Add filter item and document counts method to search.js

--- routes/search.js
+++ routes/search.js
@@ -6,6 +6,8 @@ const router = express.Router()
 const mongoose = require('mongoose')
 // Include Restaurant Model
 const Restaurant = require('../models/restaurant')
+// Include customize modules
+const { getRatingCount } = require('../data-process')
 
 // searching restaurant
 router.get('/', (req, res) => {
@@ -14,28 +16,53 @@ router.get('/', (req, res) => {
   const updatedInput = req.query.keyword.replace(/\W/g, replacer)
   // Define regular expression 
   const regex = new RegExp(updatedInput, 'i')
-  Restaurant.find({ $or: [{ name: regex }, { category: regex }] }, (err, restaurants) => {
-    if (err) return console.error(err)
-    let emptyData = restaurants.length === 0 ? true : false
-    return res.render('index', { restaurants, searchInput: req.query.keyword, emptyData, indexCSS: true })
-  })
+
+  Restaurant.find({})
+    //find all restaurants to count document amount of each rating range on filter panel
+    .then(restaurants => {
+      rating = getRatingCount(restaurants)
+      return Restaurant.find({ $or: [{ name: regex }, { category: regex }] })
+    })
+    // then find restaurants with filter criteria to display
+    .then(restaurants => {
+      let emptyData = restaurants.length === 0 ? true : false
+      res.render('index', { restaurants, searchInput: req.query.keyword, emptyData, indexCSS: true, rating, ratingOptions: [] })
+    })
+    // send error message if any
+    .catch(err => console.error(err))
 })
 
 // searching restaurant by keyword
 router.get('/category/:category', (req, res) => {
-  Restaurant.find({ category: req.params.category }, (err, restaurants) => {
-    if (err) return console.error(err)
-    let emptyData = restaurants.length === 0 ? true : false
-    return res.render('index', { restaurants, searchInput: req.params.category, emptyData, indexCSS: true })
-  })
+  Restaurant.find({})
+    //find all restaurants to count document amount of each rating range on filter panel
+    .then(restaurants => {
+      rating = getRatingCount(restaurants)
+      return Restaurant.find({ category: req.params.category })
+    })
+    // then find restaurants with filter criteria to display
+    .then(restaurants => {
+      let emptyData = restaurants.length === 0 ? true : false
+      res.render('index', { restaurants, searchInput: req.params.category, emptyData, indexCSS: true, rating, ratingOptions: [] })
+    })
+    // send error message if any
+    .catch(err => console.error(err))
 })
 
 router.get('/rating/:rating', (req, res) => {
-  Restaurant.find({ rating: { $gte: req.params.rating } }, (err, restaurants) => {
-    if (err) return console.error(err)
-    let emptyData = restaurants.length === 0 ? true : false
-    return res.render('index', { restaurants, emptyData, indexCSS: true })
-  })
+  Restaurant.find({})
+    //find all restaurants to count document amount of each rating range on filter panel
+    .then(restaurants => {
+      rating = getRatingCount(restaurants)
+      return Restaurant.find({ rating: { $gte: req.params.rating } })
+    })
+    // then find restaurants with filter criteria to display
+    .then(restaurants => {
+      let emptyData = restaurants.length === 0 ? true : false
+      res.render('index', { restaurants, emptyData, indexCSS: true, rating, ratingOptions: [] })
+    })
+    // send error message if any
+    .catch(err => console.error(err))
 })
 
 module.exports = router
