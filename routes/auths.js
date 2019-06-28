76db4deef52d68e4dc114483fc1b4d2bd20dd200
Mike <mike.huang.mikank@gmail.com>
2019-06-28 15:34:07 +0800

    Add comment in Facebook and Google authenticaion routes

--- routes/auths.js
+++ routes/auths.js
@@ -2,15 +2,19 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 
+// Facebook authentication route
 router.get('/facebook', passport.authenticate('facebook', { scope: ['email', 'public_profile'] }))
 
+// Facebook authentication callback route
 router.get('/facebook/callback', passport.authenticate('facebook', {
   successRedirect: '/',
   failureRedirect: '/users/login'
 }))
 
+// Google authentication route
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
 
+// Google authentication callback route
 router.get('/google/callback', passport.authenticate('google', {
   successRedirect: '/',
   failureRedirect: '/users/login'
